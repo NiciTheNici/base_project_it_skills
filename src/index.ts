4422c6b36a3645c387b6403efccb42d218f4639d
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-05-09 16:38:14 +0200

    remove deprecated import

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+import { DataSource } from "typeorm"
+
+
+
+const main = async() => {
+  const db = new DataSource({
+    type: "postgres", 
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    database: "base_project",
+  });
+
+  try {
+    db.initialize();
+    console.log("Connected to database");
+  } 
+  catch (error) {
+    throw new Error("Could not connect to database");
+  }
+}
+
+main();
