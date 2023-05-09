de290edcd9b0c090f9e39db16880295761a4ab1e
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-05-09 20:26:47 +0200

    create express website app something?

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { User } from "./entities/user";
 import { Animal } from "./entities/animal";
 import { Pet } from "./entities/pet";
 import { Console } from "./entities/console";
+import express from "express";
 
 
+const app = express();
 
 const main = async() => {
   const db = new DataSource({
@@ -25,10 +27,15 @@ const main = async() => {
   try {
     db.initialize();
     console.log("Connected to database");
+
+    app.listen(8080, () => {
+      console.log("Now running on port 8080")
+    })
   } 
   catch (error) {
     throw new Error("Could not connect to database");
   }
+
 }
 
 main();
