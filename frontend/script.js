89e616141df868c453efea00e27f6a12a741fba7
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-05-11 17:27:04 +0200

    add html form to add and get all persons

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,15 @@
 console.log("hello world");
 
 async function addPerson() {
+  let username = document.getElementById("username").value;
+  let first_name = document.getElementById("first_name").value;
+  let email = document.getElementById("email").value;
+  let age = document.getElementById("age").value;
   let json = JSON.stringify({
-      username: "Nici",
-      first_name: "Nicolas",
-      email: "nicolas@example.com",
-      age: 17,
+      username: username,
+      first_name: first_name,
+      email: email,
+      age: age,
       keyboards: ["Discipline v1", "Ducky One 2 Mini", "Discipline v2"]
     })
   let response = await fetch("http://localhost:8080/api/person", {
